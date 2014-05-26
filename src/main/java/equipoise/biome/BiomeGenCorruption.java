ed73f67b6a050a6e7cfdab9b66dee9ef4a85f2c9
Skymmer <skymmermc@gmail.com>
2014-05-26 16:33:14 -0500

    added placeholder biome files

---
+++ src/main/java/equipoise/biome/BiomeGenCorruption.java
@@ -0,0 +1,11 @@
+package equipoise.biome;
+
+import net.minecraft.world.biome.BiomeGenBase;
+
+public class BiomeGenCorruption extends BiomeGenBase {
+
+	public BiomeGenCorruption(int id) {
+		super(id);
+	}
+	
+}
---
+++ src/main/java/equipoise/biome/BiomeGenPurity.java
@@ -0,0 +1,11 @@
+package equipoise.biome;
+
+import net.minecraft.world.biome.BiomeGenBase;
+
+public class BiomeGenPurity extends BiomeGenBase {
+
+	public BiomeGenPurity(int id) {
+		super(id);
+	}
+
+}
